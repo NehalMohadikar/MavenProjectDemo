d1e3c6f9e011e4da860d6e66542d4a22176029b4
Nehal Mohadikar <165059150+NehalMohadikar@users.noreply.github.com>
2024-05-01 13:50:00 +0530

    Add files via upload
    Repo Update

---
+++ src/test/java/com/qa/automation/FirstTest.java
@@ -0,0 +1,25 @@
+package com.qa.automation;
+
+import org.testng.annotations.Test;
+
+public class FirstTest {
+	
+	@Test
+	public void test1() {
+		
+		System.out.println("Running===FirtTest===test1");
+	}
+	
+	@Test
+	public void test2() {
+		
+		System.out.println("Running===FirstTest===test2");
+	}
+	
+	@Test
+	public void test3() {
+		
+		System.out.println("Running===FirstTest===test3");
+	}
+
+}
---
+++ src/test/java/com/qa/automation/SecondTest.java
@@ -0,0 +1,25 @@
+package com.qa.automation;
+
+import org.testng.annotations.Test;
+
+public class SecondTest {
+	
+	@Test
+	public void test1() {
+		
+		System.out.println("Running===SecondTest===test1");
+	}
+	
+	@Test
+	public void test2() {
+		
+		System.out.println("Running===SecondTest===test2");
+	}
+	
+	@Test
+	public void test3() {
+		
+		System.out.println("Running===SecondTest===test3");
+	}
+
+}
